9ecb9e70eae32515f272cd2613362f17fbb1b69b
Bitha17 <16521076@mahasiswa.itb.ac.id>
2024-05-14 14:12:34 +0700

    feat oblique camera

---
+++ src/src/camera/ObliqueCamera.js
@@ -0,0 +1,29 @@
+import { Matrix4 } from "../math/matrix4.js";
+import { Camera } from "./index.js";
+
+export class ObliqueCamera extends Camera {
+    constructor(left, right, top, bottom, near, far, angle=45) {
+        super();
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+        this.near = near;
+        this.far = far;
+        this.angle = angle;
+        this.computeProjectionMatrix();
+    }
+
+    get type() {
+        return "ObliqueCamera";
+    }
+
+    computeProjectionMatrix() {
+        this.projectionMatrix = Matrix4.oblique(
+            this.left, this.right, 
+            this.bottom, this.top, 
+            this.near, this.far,
+            this.angle, 0.5, 1.0,
+        );
+    }
+}
--- src/src/math/matrix4.js
+++ src/src/math/matrix4.js
@@ -14,4 +14,34 @@ export class Matrix4 extends Matrix {
       [0, 0, 0, 1],
     ]);
   }
+
+  static ortographic(left, right, bottom, top, near, far) {
+
+  }
+
+  static oblique(left, right, bottom, top, near, far, angle, scale=0.5, zoom=1.0) {
+    angle *= DEG2RAD;
+    const d = [
+        (right - left) / (2 * zoom),
+        (top - bottom) / (2 * zoom),
+        (right - left) / 2,
+        (top - bottom) / 2,
+    ];
+    const border = [
+        -(d[2] + d[0])/2,
+        (d[2] + d[0])/2,
+        -(d[3] + d[1])/2,
+        (d[3] + d[1])/2,
+    ];
+    return Matrix4.premul(
+        Matrix4.orthographic(...border, near, far),
+        new Matrix4([
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            -scale * Math.cos(angle), scale * Math.sin(angle), 1, 0,
+            0, 0, 0, 1,
+        ]),
+    );
+}
+
 }
