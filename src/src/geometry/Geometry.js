4c1d93736921c2e1a3c450df7417efb2adf05c69
Bitha17 <16521076@mahasiswa.itb.ac.id>
2024-05-17 15:32:11 +0700

    feat: geometry size

--- src/src/geometry/Geometry.js
+++ src/src/geometry/Geometry.js
@@ -2,21 +2,26 @@ import { BufferAttribute } from "./BufferAttribute.js";
 import { BufferGeometry } from "./BufferGeometry.js";
 
 export class Geometry extends BufferGeometry {
-  constructor(vertices, position = { x: 0, y: 0, z: 0 }) {
+  constructor(vertices, position = { x: 0, y: 0, z: 0 }, size = { x: 1, y: 1, z: 1 }) {
     super();
     
     this.position = position;
+    this.size = size;
 
     const px = position.x,
           py = position.y,
           pz = position.z;
 
-    // Adjust vertices based on the provided center position
+    const sx = size.x,
+          sy = size.y,
+          sz = size.z;
+
+    // Adjust vertices based on the provided center position and size
     const adjustedVertices = new Float32Array(vertices.length);
     for (let i = 0; i < vertices.length; i += 3) {
-      adjustedVertices[i] = vertices[i] + px;
-      adjustedVertices[i + 1] = vertices[i + 1] + py;
-      adjustedVertices[i + 2] = vertices[i + 2] + pz;
+      adjustedVertices[i] = vertices[i] * sx + px;
+      adjustedVertices[i + 1] = vertices[i + 1] * sy + py;
+      adjustedVertices[i + 2] = vertices[i + 2] * sz + pz;
     }
 
     this.setAttribute("position", new BufferAttribute(adjustedVertices, 3));
@@ -29,13 +34,15 @@ export class Geometry extends BufferGeometry {
     return {
       ...parent,
       position: this.position,
+      size: this.size,
       type: "Geometry",
     };
   }
 
   static fromJSON(json, geometry = null) {
-    if (!geometry)
-      geometry = new Geometry(new Float32Array(json.vertices), json.position);
+    if (!geometry) {
+      geometry = new Geometry(new Float32Array(json.vertices), json.position, json.size);
+    }
     super.fromJSON(json, geometry);
     return geometry;
   }
