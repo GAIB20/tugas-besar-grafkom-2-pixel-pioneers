c9b089b0521fe57e2eb3ee72a92a3180a1594dcc
Bitha17 <16521076@mahasiswa.itb.ac.id>
2024-05-22 23:24:51 +0700

    edit cat

--- src/src/models/articulated/cat.js
+++ src/src/models/articulated/cat.js
@@ -19,27 +19,29 @@ var cat = {
           },
           PEarL: {
             options: {
-              position: [-0.3, 0.65, 0.2],
+              position: [-0.3, 0.5, 0.25],
               scale: [0.3,0.3,0.3],
+              rotation:[1,0,0],
             },
           },
           PEarR: {
             options: {
-              position: [-0.3, 0.65, -0.2],
+              position: [-0.3, 0.5, -0.25],
               scale: [0.3,0.3,0.3],
+              rotation:[1,0,0],
             },
           },
         },
       },
       RArmL: {
         options: {
-          position: [-1.2, -0.5, 0.5],
+          position: [-1.2, -0.5, 0.4],
         },
         children: {
           PArmL: {
             options: {
               position: [0, 0, 0],
-              scale: [0.5, 1, 0.5],
+              scale: [0.3, 1, 0.3],
               rotation: [0, 0, 0],
             },
           },
@@ -47,13 +49,13 @@ var cat = {
       },
       RArmR: {
         options: {
-          position: [-1.2, -0.5, -0.5],
+          position: [-1.2, -0.5, -0.4],
         },
         children: {
           PArmR: {
             options: {
               position: [0, 0, 0],
-              scale: [0.5, 1, 0.5],
+              scale: [0.3, 1, 0.3],
               rotation: [0, 0, 0],
             },
           },
@@ -61,13 +63,13 @@ var cat = {
       },
       RLegL: {
         options: {
-          position: [1.2, -0.5, 0.5],
+          position: [1.2, -0.5, 0.4],
         },
         children: {
           PLegL: {
             options: {
               position: [0, 0, 0],
-              scale: [0.5, 1, 0.5],
+              scale: [0.3, 1, 0.3],
               rotation: [0, 0, 0],
             },
           },
@@ -75,13 +77,13 @@ var cat = {
       },
       RLegR: {
         options: {
-          position: [1.2, -0.5, -0.5],
+          position: [1.2, -0.5, -0.4],
         },
         children: {
           PLegR: {
             options: {
               position: [0, 0, 0],
-              scale: [0.5, 1, 0.5],
+              scale: [0.3, 1, 0.3],
               rotation: [0, 0, 0],
             },
           },
@@ -89,13 +91,13 @@ var cat = {
       },
       RTail: {
         options: {
-          position: [1.25, 0.25, 0.5]
+          position: [1.25, 0.25, 0]
         },
         children: {
           PTail: {
             options: {
               position: [0.75,0,0],
-              scale: [1.5,0.5,0.5],
+              scale: [1.5,0.3,0.3],
               rotation: [0,0,0.2]
             },
           }
