3ff5a265823e56576ea33b53f7615908c53004a1
maikeljh <mjh191003@gmail.com>
2024-05-26 17:45:37 +0700

    [New Feature] Add test folder

---
+++ test/animations/fishAnimation.js
@@ -0,0 +1,327 @@
+var fish = {
+  name: "Fish Animation",
+  frames: [
+    // 0
+    {
+      RBody: {
+        position: [-1, 0.2, -0.363],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, -0.22, 0.51],
+        rotation: [-0.5, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.93],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.93],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 1
+    {
+      RBody: {
+        position: [-1, 0.2, 0],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, 0, 0.51],
+        rotation: [-0.7, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0.5, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0.1, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.7],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.7],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 2
+    {
+      RBody: {
+        position: [-1, 0.2, 0.3],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, -0.22, 0.51],
+        rotation: [-0.5, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.93],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.93],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 3
+    {
+      RBody: {
+        position: [-1, 0.2, 0.6],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, 0, 0.51],
+        rotation: [-1.3, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, -0.5, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, -0.1, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.7],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.7],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 4
+    {
+      RBody: {
+        position: [-1, 0.2, 0.9],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, -0.22, 0.51],
+        rotation: [-0.5, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.93],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.93],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 5
+    {
+      RBody: {
+        position: [-1, 0.2, 1.2],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, 0, 0.51],
+        rotation: [-0.7, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0.5, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0.1, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.7],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.7],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 6
+    {
+      RBody: {
+        position: [-1, 0.2, 1.5],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, -0.22, 0.51],
+        rotation: [-0.5, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.93],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.93],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 7
+    {
+      RBody: {
+        position: [-1, 0.2, 1.8],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, 0, 0.51],
+        rotation: [-1.3, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, -0.5, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, -0.1, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.7],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.7],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 8
+    {
+      RBody: {
+        position: [-1, 0.2, 2.1],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, -0.22, 0.51],
+        rotation: [-0.5, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.93],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.93],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+    // 9
+    {
+      RBody: {
+        position: [-1, 0.2, 2.4],
+      },
+      RTopHead: {
+        position: [-1, 0.43, 0.71],
+      },
+      RBottomHead: {
+        position: [-1, 0, 0.51],
+        rotation: [-0.7, 0, 0],
+      },
+      RPreTail: {
+        position: [-1, 0.2, -1.2],
+        rotation: [0, 0.5, 0],
+      },
+      RTail: {
+        position: [0, 0, -0.4],
+        rotation: [0, 0.1, 0],
+      },
+      RRightFin: {
+        position: [-0.4, -0.026, -0.143],
+        rotation: [0, 0, 22.7],
+      },
+      RLeftFin: {
+        position: [-1.6, -0.026, -0.143],
+        rotation: [0, 0, -22.7],
+      },
+      RTopFin: {
+        position: [-1, 0.75, -0.413],
+      },
+    },
+  ]
+};
+
+export default fish;
---
+++ test/animations/objAnimation.js
@@ -0,0 +1,366 @@
+export default {
+    name: "Obj Animation",
+    frames: [
+      {
+        RBody: {
+          position: [0, 0.1, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.1, 0.5],
+          rotation: [0.2, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.1, -0.5],
+          rotation: [-0.2, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.3],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.2, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.2, 0.5],
+          rotation: [0.4, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.2, -0.5],
+          rotation: [-0.4, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.25],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.3, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.3, 0.5],
+          rotation: [0.6, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.3, -0.5],
+          rotation: [-0.6, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.2],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.4, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.4, 0.5],
+          rotation: [0.8, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.4, -0.5],
+          rotation: [-0.8, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.15],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.5, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.5, 0.5],
+          rotation: [1, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.5, -0.5],
+          rotation: [-1, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.1],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.4, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.4, 0.5],
+          rotation: [0.8, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.4, -0.5],
+          rotation: [-0.8, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.05],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.3, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.3, 0.5],
+          rotation: [0.6, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.3, -0.5],
+          rotation: [-0.6, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.2, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.2, 0.5],
+          rotation: [0.4, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.2, -0.5],
+          rotation: [-0.4, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.5],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0.1, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, -0.1, 0.5],
+          rotation: [0.2, 0, 0],
+        },
+        RWingR: {
+          position: [0, -0.1, -0.5],
+          rotation: [-0.2, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.1],
+        }
+      },
+      {
+        RBody: {
+          position: [0, 0, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0, 0.5],
+          rotation: [0, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0, -0.5],
+          rotation: [0, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.15],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.1, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.1, 0.5],
+          rotation: [-0.2, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.1, -0.5],
+          rotation: [0.2, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.2],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.2, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.2, 0.5],
+          rotation: [-0.4, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.2, -0.5],
+          rotation: [0.4, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.25],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.3, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.3, 0.5],
+          rotation: [-0.6, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.3, -0.5],
+          rotation: [0.6, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.3],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.4, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.4, 0.5],
+          rotation: [-0.8, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.4, -0.5],
+          rotation: [0.8, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.25],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.5, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.5, 0.5],
+          rotation: [-1, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.5, -0.5],
+          rotation: [1, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.2],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.4, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.4, 0.5],
+          rotation: [-0.8, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.4, -0.5],
+          rotation: [0.8, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.15],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.3, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.3, 0.5],
+          rotation: [-0.6, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.3, -0.5],
+          rotation: [0.6, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.2, 0],
+            rotation: [0, 0, 0.1],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.2, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.2, 0.5],
+          rotation: [-0.4, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.2, -0.5],
+          rotation: [0.4, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.05],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, -0.1, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0.1, 0.5],
+          rotation: [-0.2, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0.1, -0.5],
+          rotation: [0.2, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.15],
+        }
+      }, 
+      {
+        RBody: {
+          position: [0, 0, 0],
+          rotation: [0, 0, 0],
+        },
+        RWingL: {
+          position: [0, 0, 0.5],
+          rotation: [0, 0, 0],
+        },
+        RWingR: {
+          position: [0, 0, -0.5],
+          rotation: [0, 0, 0],
+        },
+        RTail: {
+            position: [1.25, 0.20, 0],
+            rotation: [0, 0, 0.25],
+        }
+      }, 
+    ],
+  };
+  
